c150950abc4e1d0c0342f794f4c2fee09a599eda
theforps <theforps@gmail.com>
2023-08-27 20:22:35 +0300

    Hi :)

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Create object of class and start Task
+        Class object = new Class();
+        */
+
+
+
+    }
+}
