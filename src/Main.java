d1abb78b17853ec8cbdbefdf1146ff7ee8c8b6d3
theforps <theforps@gmail.com>
2023-08-27 21:26:14 +0300

    :3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import Tasks.*;
+
 public class Main {
     public static void main(String[] args) {
         /*
         Create object of class and start Task
         Class object = new Class();
         */
-
 
 
     }
