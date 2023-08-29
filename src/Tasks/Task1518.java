abbd2f8eb9997b3a08ed78fc863bcc47be2d061e
theforps <theforps@gmail.com>
2023-08-29 22:51:24 +0300

    Task 1518 completed

---
+++ src/Tasks/Task1518.java
@@ -0,0 +1,23 @@
+package Tasks;
+
+//https://leetcode.com/problems/water-bottles
+
+public class Task1518 {
+    public Task1518(int numBottles, int numExchange)
+    {
+        System.out.println(numWaterBottles(numBottles, numExchange));
+    }
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+
+        int count = numBottles;
+
+        while (numBottles >= numExchange)
+        {
+            count += numBottles / numExchange;
+            numBottles = numBottles % numExchange + numBottles / numExchange;
+        }
+
+        return count;
+    }
+}
