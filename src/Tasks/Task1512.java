9fa701da88ec59038d913390008fde8a95ad8693
theforps <theforps@gmail.com>
2023-08-30 13:10:19 +0300

    Task 1512 completed

---
+++ src/Tasks/Task1512.java
@@ -0,0 +1,33 @@
+package Tasks;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+//https://leetcode.com/problems/number-of-good-pairs
+
+public class Task1512 {
+    public Task1512(int[] nums)
+    {
+        System.out.println(numIdenticalPairs(nums));
+    }
+
+    public int numIdenticalPairs(int[] nums) {
+
+        HashSet<String> pairs = new HashSet<>();
+
+        for(int i = 0; i<nums.length; i++)
+        {
+            for(int j = 0; j<nums.length; j++)
+            {
+                if(nums[i] == nums[j] && i < j)
+                {
+                    char[] temp = {(char)nums[i], (char)nums[j]};
+                    Arrays.sort(temp);
+
+                    pairs.add(temp.toString());
+                }
+            }
+        }
+        return pairs.size();
+    }
+}
