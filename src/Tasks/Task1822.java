9f211da6f0ada8bd517b945039ea6c59b3c3db48
theforps <theforps@gmail.com>
2023-08-30 13:24:39 +0300

    Task 1822 completed

---
+++ src/Tasks/Task1822.java
@@ -0,0 +1,24 @@
+package Tasks;
+
+//https://leetcode.com/problems/sign-of-the-product-of-an-array
+
+public class Task1822 {
+    public Task1822(int[] nums)
+    {
+        System.out.println(arraySign(nums));
+    }
+
+    public int arraySign(int[] nums) {
+        double product = 1;
+
+        for(var x : nums)
+            product *= x;
+
+        if(product > 0)
+            return 1;
+        else if(product < 0)
+            return -1;
+
+        return 0;
+    }
+}
