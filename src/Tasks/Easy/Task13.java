050fac6f2e432e1fbce63c5c36fe8d2fddcc6667
theforps <theforps@gmail.com>
2023-08-31 23:39:58 +0300

    Completed

---
+++ src/Tasks/Easy/Task13.java
@@ -0,0 +1,35 @@
+package Tasks.Easy;
+
+import java.util.HashMap;
+
+public class Task13 {
+    public Task13(String s)
+    {
+        System.out.println(romanToInt(s));
+    }
+
+    public int romanToInt(String s) {
+        int result = 0;
+        HashMap<Character, Integer> dictionary = new HashMap<>(){{
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }};
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(i+1 < s.length() && dictionary.get(s.charAt(i)) <  dictionary.get(s.charAt(i+1)))
+            {
+                result -= dictionary.get(s.charAt(i));
+            }
+            else {
+                result += dictionary.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+}
