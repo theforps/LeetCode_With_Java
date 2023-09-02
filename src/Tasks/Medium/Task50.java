5eaac91d6678af538881c742c2be17d889b08f91
theforps <theforps@gmail.com>
2023-09-02 15:27:01 +0300

    Completed

---
+++ src/Tasks/Medium/Task50.java
@@ -0,0 +1,13 @@
+package Tasks.Medium;
+
+//https://leetcode.com/problems/powx-n
+
+public class Task50 {
+    public Task50(double x, int n){
+        System.out.println(myPow(x, n));
+    }
+
+    public double myPow(double x, int n) {
+        return Math.pow(x, n);
+    }
+}
