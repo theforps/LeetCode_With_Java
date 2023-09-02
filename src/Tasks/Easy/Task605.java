ce016b8679e757aee0bd6d62618bb4e94f474614
theforps <theforps@gmail.com>
2023-09-02 17:05:16 +0300

    Completed

---
+++ src/Tasks/Easy/Task605.java
@@ -0,0 +1,31 @@
+package Tasks.Easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task605 {
+    public Task605(int[] flowerbed, int n)
+    {
+        System.out.println(canPlaceFlowers(flowerbed, n));
+    }
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        List<Integer> list = new ArrayList<>();
+        for(var x : flowerbed)
+            list.add(x);
+        list.add(0);
+
+        for(int i = 0; i < list.size()-1; i+=2)
+        {
+            if(list.get(i) == 0 && list.get(i+1) == 0)
+            {
+                n--;
+            }
+            else if(list.get(i+1) == 1)
+                i++;
+        }
+        return n <= 0;
+    }
+}
