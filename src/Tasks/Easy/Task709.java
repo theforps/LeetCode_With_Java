8fe306d5d457a97138fee238e08fb6fb01d37f1d
theforps <theforps@gmail.com>
2023-09-03 21:48:43 +0300

    Completed

---
+++ src/Tasks/Easy/Task709.java
@@ -0,0 +1,15 @@
+package Tasks.Easy;
+
+public class Task709 {
+    public Task709(String s)
+    {
+        System.out.println(toLowerCase(s));
+    }
+
+    public String toLowerCase(String s) {
+
+        s = s.toLowerCase();
+
+        return s;
+    }
+}
