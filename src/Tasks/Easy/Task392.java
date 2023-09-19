6899aa5b38251255f3f4ef97aa57330d6daca230
theforps <theforps@gmail.com>
2023-09-19 11:07:33 +0300

    Completed

--- src/Tasks/Easy/Task392.java
+++ src/Tasks/Easy/Task392.java
@@ -1,26 +1,24 @@
 package Tasks.Easy;
 
-import java.util.Arrays;
+//https://leetcode.com/problems/is-subsequence
 
 public class Task392 {
     public Task392(String s, String t)
     {
         System.out.println(isSubsequence(s, t));
     }
-
     public boolean isSubsequence(String s, String t) {
+        int temp = 0, result = 0;
 
-        char[] arr1 = t.toCharArray();
-        String result = "";
+        for(int i = 0; i < s.length(); i++)
+            for(int j = temp; j < t.length(); j++)
+                if(s.charAt(i) == t.charAt(j))
+                {
+                    result++;
+                    temp = j + 1;
+                    break;
+                }
 
-        for(int i = 0; i < arr1.length; i++)
-        {
-            if(s.contains(Character.toString(arr1[i])))
-            {
-                result += arr1[i];
-            }
-        }
-
-        return s.equals(result);
+        return result == s.length();
     }
 }
