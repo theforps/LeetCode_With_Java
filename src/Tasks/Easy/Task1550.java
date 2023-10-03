8a271faae1383b77875c5e0ca701c3b1de362b82
theforps <theforps@gmail.com>
2023-10-03 20:34:12 +0300

    Completed

---
+++ src/Tasks/Easy/Task1550.java
@@ -0,0 +1,20 @@
+package Tasks.Easy;
+
+public class Task1550 {
+    public Task1550(int[] arr)
+    {
+        System.out.println(threeConsecutiveOdds(arr));
+    }
+
+    public boolean threeConsecutiveOdds(int[] arr) {
+
+        if(arr.length >= 3)
+
+            for(int i = 2; i<arr.length; i++)
+
+                if(arr[i] % 2 == 1 && arr[i-1] % 2 == 1 && arr[i-2] % 2 == 1)
+                    return true;
+
+        return false;
+    }
+}
